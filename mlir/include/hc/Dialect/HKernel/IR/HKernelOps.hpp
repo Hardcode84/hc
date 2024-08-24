39ac2a1063c73173120b78fe32484d73566a9057
Ivan Butygin <ivan.butygin@gmail.com>
2024-08-25 01:45:28 +0200

    Kernel dialect stub

---
+++ mlir/include/hc/Dialect/HKernel/IR/HKernelOps.hpp
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#pragma once
+
+#include <mlir/IR/Dialect.h>
+#include <mlir/IR/OpDefinition.h>
+#include <mlir/IR/OpImplementation.h>
+
+#include "hc/Dialect/Typing/IR/TypingOpsInterfaces.hpp"
+
+#include "hc/Dialect/HKernel/IR/HKernelOpsDialect.h.inc"
+#include "hc/Dialect/HKernel/IR/HKernelOpsEnums.h.inc"
+
+#define GET_TYPEDEF_CLASSES
+#include "hc/Dialect/HKernel/IR/HKernelOpsTypes.h.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "hc/Dialect/HKernel/IR/HKernelOpsAttributes.h.inc"
+
+#define GET_OP_CLASSES
+#include "hc/Dialect/HKernel/IR/HKernelOps.h.inc"
