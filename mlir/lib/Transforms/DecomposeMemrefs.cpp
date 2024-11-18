2926cf268b4176ac7894f53c2fdb62851c112f59
Ivan Butygin <ivan.butygin@gmail.com>
2024-11-18 04:39:15 +0100

    memref.subview decompose

--- mlir/lib/Transforms/DecomposeMemrefs.cpp
+++ mlir/lib/Transforms/DecomposeMemrefs.cpp
@@ -6,9 +6,12 @@
 
 #include "hc/Dialect/HKernel/IR/HKernelOps.hpp"
 
+#include <mlir/Dialect/Affine/IR/AffineOps.h>
 #include <mlir/Dialect/Arith/IR/Arith.h>
+#include <mlir/Dialect/Arith/Utils/Utils.h>
 #include <mlir/Dialect/Func/IR/FuncOps.h>
 #include <mlir/Dialect/MemRef/IR/MemRef.h>
+#include <mlir/Dialect/Utils/IndexingUtils.h>
 #include <mlir/Transforms/DialectConversion.h>
 
 namespace hc {
@@ -50,6 +53,63 @@ static void populateTypeConverter(mlir::MLIRContext *ctx,
       });
 }
 
+static mlir::LogicalResult getStrides(mlir::MemRefType type,
+                                      llvm::SmallVectorImpl<int64_t> &strides) {
+  int64_t offset; // unused
+  return mlir::getStridesAndOffset(type, strides, offset);
+}
+
+static mlir::LogicalResult
+materializeStrides(mlir::OpBuilder &builder, mlir::Location loc,
+                   mlir::MemRefType type, mlir::Value srcPacked,
+                   llvm::SmallVectorImpl<mlir::OpFoldResult> &ret) {
+  auto tupleType = mlir::dyn_cast<mlir::TupleType>(srcPacked.getType());
+  int currentIndex = 1; // 0th is ptr
+  auto getNextVal = [&]() -> mlir::OpFoldResult {
+    mlir::Value idx =
+        builder.create<mlir::arith::ConstantIndexOp>(loc, currentIndex);
+    auto elemType = tupleType.getType(currentIndex);
+    ++currentIndex;
+    return builder.create<hc::hk::TupleExtractOp>(loc, elemType, srcPacked, idx)
+        .getResult();
+  };
+
+  auto createConst = [&](int64_t val) -> mlir::OpFoldResult {
+    return builder.getIndexAttr(val);
+  };
+
+  auto getDim = [&](int64_t val) -> mlir::OpFoldResult {
+    if (mlir::ShapedType::isDynamic(val)) {
+      return getNextVal();
+    } else {
+      return createConst(val);
+    }
+  };
+
+  if (type.getLayout().isIdentity()) {
+    mlir::Value stride =
+        mlir::getValueOrCreateConstantIndexOp(builder, loc, createConst(1));
+    ret.emplace_back(stride);
+    for (auto s : llvm::reverse(type.getShape().drop_front())) {
+      mlir::Value dimVal =
+          mlir::getValueOrCreateConstantIndexOp(builder, loc, getDim(s));
+      stride = builder.create<mlir::arith::MulIOp>(loc, stride, dimVal);
+      ret.emplace_back(stride);
+    }
+    std::reverse(ret.end() - type.getRank(), ret.end()); // inplace
+  } else {
+    llvm::SmallVector<int64_t> strides;
+    if (mlir::failed(getStrides(type, strides)))
+      return mlir::failure();
+
+    currentIndex += type.getNumDynamicDims();
+    for (auto s : strides) {
+      ret.emplace_back(getDim(s));
+    }
+  }
+  return mlir::success();
+}
+
 namespace {
 struct ConvertAlloca final : mlir::OpConversionPattern<mlir::memref::AllocaOp> {
   using OpConversionPattern::OpConversionPattern;
@@ -85,6 +145,88 @@ struct ConvertAlloca final : mlir::OpConversionPattern<mlir::memref::AllocaOp> {
     llvm::SmallVector<mlir::Value> results;
     results.emplace_back(ptr);
     llvm::append_range(results, adaptor.getDynamicSizes());
+    rewriter.replaceOpWithNewOp<hc::hk::MakeTupleOp>(op, resultType, results);
+    return mlir::success();
+  }
+};
+
+struct ConvertSubview final
+    : mlir::OpConversionPattern<mlir::memref::SubViewOp> {
+  using OpConversionPattern::OpConversionPattern;
+
+  mlir::LogicalResult
+  matchAndRewrite(mlir::memref::SubViewOp op, OpAdaptor adaptor,
+                  mlir::ConversionPatternRewriter &rewriter) const override {
+    mlir::MemRefType srcMemrefType = op.getSourceType();
+    mlir::MemRefType resMemrefType = op.getType();
+    auto resultType =
+        getTypeConverter()->convertType<mlir::TupleType>(resMemrefType);
+    if (!resultType)
+      return rewriter.notifyMatchFailure(op, "Unable to convert result type");
+
+    llvm::SmallVector<int64_t> outStrides;
+    if (mlir::failed(getStrides(resMemrefType, outStrides)))
+      return rewriter.notifyMatchFailure(op,
+                                         "Unable to convert result strides");
+
+    mlir::Location loc = op.getLoc();
+    llvm::SmallVector<mlir::OpFoldResult> strides;
+    if (mlir::failed(materializeStrides(rewriter, loc, srcMemrefType,
+                                        adaptor.getSource(), strides)))
+      return rewriter.notifyMatchFailure(op, "Unable to materialize strides");
+
+    auto mixedOffsets = mlir::getMixedValues(adaptor.getStaticOffsets(),
+                                             adaptor.getOffsets(), rewriter);
+    auto mixedSizes = mlir::getMixedValues(adaptor.getStaticSizes(),
+                                           adaptor.getSizes(), rewriter);
+    auto mixedStrides = mlir::getMixedValues(adaptor.getStaticStrides(),
+                                             adaptor.getStrides(), rewriter);
+    auto &&[expr, values] = mlir::computeLinearIndex(rewriter.getIndexAttr(0),
+                                                     strides, mixedOffsets);
+    mlir::OpFoldResult finalOffset =
+        mlir::affine::makeComposedFoldedAffineApply(rewriter, loc, expr,
+                                                    values);
+    mlir::Value finalOffsetVal =
+        mlir::getValueOrCreateConstantIndexOp(rewriter, loc, finalOffset);
+
+    mlir::Type ptrType = resultType.getType(0);
+    mlir::Value zero = rewriter.create<mlir::arith::ConstantIndexOp>(loc, 0);
+    mlir::Value ptr = rewriter.create<hc::hk::TupleExtractOp>(
+        loc, ptrType, adaptor.getSource(), zero);
+    ptr = rewriter.create<hc::hk::PtrAddOp>(loc, ptrType, ptr, finalOffsetVal);
+
+    llvm::SmallVector<mlir::Value> results;
+    results.emplace_back(ptr);
+
+    auto droppedDims = op.getDroppedDims();
+    for (auto &&[i, dim, dynDim] :
+         llvm::enumerate(resMemrefType.getShape(), mixedSizes)) {
+      if (droppedDims[i] || !mlir::ShapedType::isDynamic(dim))
+        continue;
+
+      results.emplace_back(
+          mlir::getValueOrCreateConstantIndexOp(rewriter, loc, dynDim));
+    }
+
+    llvm::ArrayRef outStridesRange(outStrides);
+    for (auto &&[i, dynStride] : llvm::enumerate(mixedStrides)) {
+      if (droppedDims[i])
+        continue;
+
+      auto stride = outStridesRange.front();
+      outStridesRange = outStridesRange.drop_front();
+      if (!mlir::ShapedType::isDynamic(stride))
+        continue;
+
+      mlir::Value srcStride =
+          mlir::getValueOrCreateConstantIndexOp(rewriter, loc, strides[i]);
+      mlir::Value dstStride =
+          mlir::getValueOrCreateConstantIndexOp(rewriter, loc, dynStride);
+      mlir::Value newStride =
+          rewriter.create<mlir::arith::MulIOp>(loc, srcStride, dstStride);
+      results.emplace_back(newStride);
+    }
+
     rewriter.replaceOpWithNewOp<hc::hk::MakeTupleOp>(op, resultType, results);
     return mlir::success();
   }
@@ -133,7 +275,7 @@ struct DecomposeMemrefsPass final
                                                               converter);
     patterns.insert<ConvertReturn>(converter, ctx);
 
-    patterns.insert<ConvertAlloca>(converter, ctx);
+    patterns.insert<ConvertAlloca, ConvertSubview>(converter, ctx);
 
     target.addDynamicallyLegalOp<mlir::func::FuncOp>(
         [&](mlir::func::FuncOp op) {
--- tests/Transforms/decompose-memrefs.mlir
+++ tests/Transforms/decompose-memrefs.mlir
@@ -57,3 +57,57 @@ func.func @test(%arg: index) -> memref<?xf32> {
   %0 = memref.alloca(%arg) : memref<?xf32>
   return %0 : memref<?xf32>
 }
+
+// -----
+
+//       CHECK: #[[MAP:.*]] = affine_map<()[s0, s1] -> (s0 * 7 + s1)>
+// CHECK-LABEL: func @test
+//  CHECK-SAME:  (%[[ARG0:.*]]: tuple<!hkernel.ptr<f32>>, %[[ARG1:.*]]: index, %[[ARG2:.*]]: index)
+//   CHECK-DAG:  %[[C0:.*]] = arith.constant 0 : index
+//       CHECK:  %[[OFFSET:.*]] = affine.apply #[[MAP]]()[%[[ARG1]], %[[ARG2]]]
+//       CHECK:  %[[PTR:.*]] = hkernel.tuple_extract %[[ARG0]] : tuple<!hkernel.ptr<f32>>[%[[C0]]] -> !hkernel.ptr<f32>
+//       CHECK:  %[[PTR2:.*]] = hkernel.ptr_add %[[PTR]] : !hkernel.ptr<f32>, %[[OFFSET]] : index
+//       CHECK:  %[[RES:.*]] = hkernel.make_tuple %[[PTR2]] : !hkernel.ptr<f32> -> tuple<!hkernel.ptr<f32>>
+//       CHECK:  return %[[RES]] : tuple<!hkernel.ptr<f32>>
+func.func @test(%arg0: memref<5x7xf32>, %arg1: index, %arg2: index) -> memref<2x3xf32, strided<[7, 1], offset: ?>> {
+  %1 = memref.subview %arg0[%arg1, %arg2] [2, 3] [1, 1] : memref<5x7xf32> to memref<2x3xf32, strided<[7, 1], offset: ?>>
+  return %1 : memref<2x3xf32, strided<[7, 1], offset: ?>>
+}
+
+// -----
+
+//       CHECK: #[[MAP:.*]] = affine_map<()[s0, s1, s2] -> (s0 * s1 + s2)>
+// CHECK-LABEL: func @test
+//  CHECK-SAME:  (%[[ARG0:.*]]: tuple<!hkernel.ptr<f32>, index, index>, %[[ARG1:.*]]: index, %[[ARG2:.*]]: index)
+//   CHECK-DAG:  %[[C0:.*]] = arith.constant 0 : index
+//   CHECK-DAG:  %[[C1:.*]] = arith.constant 1 : index
+//       CHECK:  %[[SIZE:.*]] = hkernel.tuple_extract %[[ARG0]] : tuple<!hkernel.ptr<f32>, index, index>[%[[C1]]] -> index
+//       CHECK:  %[[OFFSET:.*]] = affine.apply #[[MAP]]()[%[[ARG1]], %[[SIZE]], %[[ARG2]]]
+//       CHECK:  %[[PTR:.*]] = hkernel.tuple_extract %[[ARG0]] : tuple<!hkernel.ptr<f32>, index, index>[%[[C0]]] -> !hkernel.ptr<f32>
+//       CHECK:  %[[PTR2:.*]] = hkernel.ptr_add %[[PTR]] : !hkernel.ptr<f32>, %[[OFFSET]] : index
+//       CHECK:  %[[RES:.*]] = hkernel.make_tuple %[[PTR2]], %[[SIZE]] : !hkernel.ptr<f32>, index -> tuple<!hkernel.ptr<f32>, index>
+//       CHECK:  return %[[RES]] : tuple<!hkernel.ptr<f32>, index>
+func.func @test(%arg0: memref<?x?xf32>, %arg1: index, %arg2: index) -> memref<2x3xf32, strided<[?, 1], offset: ?>> {
+  %1 = memref.subview %arg0[%arg1, %arg2] [2, 3] [1, 1] : memref<?x?xf32> to memref<2x3xf32, strided<[?, 1], offset: ?>>
+  return %1 : memref<2x3xf32, strided<[?, 1], offset: ?>>
+}
+
+// -----
+
+//       CHECK: #[[MAP:.*]] = affine_map<()[s0, s1, s2, s3] -> (s0 * s1 + s2 * s3)>
+// CHECK-LABEL: func @test
+//  CHECK-SAME:  (%[[ARG0:.*]]: tuple<!hkernel.ptr<f32>, index, index, index, index>, %[[ARG1:.*]]: index, %[[ARG2:.*]]: index)
+//   CHECK-DAG:  %[[C0:.*]] = arith.constant 0 : index
+//   CHECK-DAG:  %[[C3:.*]] = arith.constant 3 : index
+//   CHECK-DAG:  %[[C4:.*]] = arith.constant 4 : index
+//       CHECK:  %[[STRIDE1:.*]] = hkernel.tuple_extract %[[ARG0]] : tuple<!hkernel.ptr<f32>, index, index, index, index>[%[[C3]]] -> index
+//       CHECK:  %[[STRIDE2:.*]] = hkernel.tuple_extract %[[ARG0]] : tuple<!hkernel.ptr<f32>, index, index, index, index>[%[[C4]]] -> index
+//       CHECK:  %[[OFFSET:.*]] = affine.apply #[[MAP]]()[%[[ARG1]], %[[STRIDE1]], %[[ARG2]], %[[STRIDE2]]]
+//       CHECK:  %[[PTR:.*]] = hkernel.tuple_extract %[[ARG0]] : tuple<!hkernel.ptr<f32>, index, index, index, index>[%[[C0]]] -> !hkernel.ptr<f32>
+//       CHECK:  %[[PTR2:.*]] = hkernel.ptr_add %[[PTR]] : !hkernel.ptr<f32>, %[[OFFSET]] : index
+//       CHECK:  %[[RES:.*]] = hkernel.make_tuple %[[PTR2]], %[[STRIDE1]], %[[STRIDE2]] : !hkernel.ptr<f32>, index, index -> tuple<!hkernel.ptr<f32>, index, index>
+//       CHECK:  return %[[RES]] : tuple<!hkernel.ptr<f32>, index, index>
+func.func @test(%arg0: memref<?x?xf32, strided<[?, ?], offset: ?>>, %arg1: index, %arg2: index) -> memref<2x3xf32, strided<[?, ?], offset: ?>> {
+  %1 = memref.subview %arg0[%arg1, %arg2] [2, 3] [1, 1] : memref<?x?xf32, strided<[?, ?], offset: ?>> to memref<2x3xf32, strided<[?, ?], offset: ?>>
+  return %1 : memref<2x3xf32, strided<[?, ?], offset: ?>>
+}
