5bf0a534334dc4fe8436ba0d07a0d58dc88f3c3b
Ivan Butygin <ivan.butygin@gmail.com>
2025-01-04 01:35:44 +0100

    py runtime func tracing

--- python/cpp/py-runtime/src/PyRuntime.cpp
+++ python/cpp/py-runtime/src/PyRuntime.cpp
@@ -7,21 +7,45 @@
 #include "PyRuntimeShared.hpp"
 #include "hc-python-runtime_export.h"
 
+namespace {
+struct FuncScope {
+  FuncScope(const char *funcName) : name(funcName), enable(true) {
+    if (enable) {
+      fprintf(stdout, "%s enter\n", name);
+      fflush(stdout);
+    }
+  }
+  FuncScope(const FuncScope &) = delete;
+  ~FuncScope() {
+    if (enable) {
+      fprintf(stdout, "%s exit\n", name);
+      fflush(stdout);
+    }
+  }
+
+private:
+  const char *name;
+  bool enable;
+};
+} // namespace
+#define LOG_FUNC() FuncScope _scope(__func__)
+
 extern "C" HC_PYTHON_RUNTIME_EXPORT int
 hcgpuConvertPyArray(hc::ExceptionDesc *errorDesc, PyObject *obj, void *ret) {
-  fprintf(stderr, "Not implemented\n");
-  fflush(stderr);
+  LOG_FUNC();
   abort();
 }
 
 extern "C" HC_PYTHON_RUNTIME_EXPORT void *
 hcgpuGetKernel(void **handle, const void *data, size_t dataSize) {
+  LOG_FUNC();
   abort();
 }
 
 extern "C" HC_PYTHON_RUNTIME_EXPORT void
 hcgpuSuggestBlockSize(void *kernel, const size_t *globalSizes,
                       size_t *blockSizesRet, size_t nDim) {
+  LOG_FUNC();
   abort();
 }
 
@@ -29,5 +53,6 @@ extern "C" HC_PYTHON_RUNTIME_EXPORT void
 hcgpuLaunchKernel(void *kernel, const size_t *gridSizes,
                   const size_t *blockSizes, size_t nDim, void **args,
                   size_t nArgs, size_t sharedMemSize) {
+  LOG_FUNC();
   abort();
 }
